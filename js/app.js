86fccfc427e7603ebfc52aa547dd348758fcb8e7
DOX7 <115791497+DarrenOxley7@users.noreply.github.com>
2022-12-16 14:26:23 +0000

    standard api version completed

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ var itemWrapper = document.querySelector('main');
 function displayMatches(matches) {
   itemWrapper.innerHTML = '';
 
+if (!matches) {
+    return itemWrapper.innerHTML = '<p> class = "no-search"> No results found</p> '
+} 
+
   for (var matchObj of matches) {
     itemWrapper.insertAdjacentHTML('beforeend', `
       <div class="movie-item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${matchObj.Poster})">
@@ -29,6 +33,7 @@ function getMovieData(event) {
     responsePromise
       .then(handleResponse)
       .then(function (data) {
+
         displayMatches(data.Search);
       });
   }
