b8fb7e126362984f19fa40e7850fb4e78f24277f
DOX7 <115791497+DarrenOxley7@users.noreply.github.com>
2022-12-16 14:58:00 +0000

    condensed to jquery

--- js/app.js
+++ js/app.js
@@ -1,46 +1,38 @@
-var searchInput = document.querySelector('.search');
-var itemWrapper = document.querySelector('main');
+var searchInput = $('.search');
+var itemWrapper = $('main');
 
 function displayMatches(matches) {
-  itemWrapper.innerHTML = '';
+  itemWrapper.html('');
 
 if (!matches) {
-    return itemWrapper.innerHTML = '<p> class = "no-search"> No results found</p> '
-} 
-
-  for (var matchObj of matches) {
-    itemWrapper.insertAdjacentHTML('beforeend', `
-      <div class="movie-item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${matchObj.Poster})">
-        <h3>${matchObj.Title}</h3>
-        <p>Release Year: ${matchObj.Year}</p>
-        <a href="https://www.imdb.com/title/${matchObj.imdbID}" target="_blank">View More Details</a>
-      </div>
-    `);
+    itemWrapper.html ('<p> class = "no-search"> No results found</p>')
+} else {
+    for (var matchObj of matches) {
+        itemWrapper.append(`
+          <div class="movie-item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${matchObj.Poster})">
+            <h3>${matchObj.Title}</h3>
+            <p>Release Year: ${matchObj.Year}</p>
+            <a href="https://www.imdb.com/title/${matchObj.imdbID}" target="_blank">View More Details</a>
+          </div>
+        `);
+        }
   }
 }
 
 function getMovieData(event) {
   var keyCode = event.keyCode;
-  var searchText = searchInput.value.trim().toLowerCase();
+  var searchText = searchInput.val().trim();
 
   if (keyCode === 13 && searchText) {
-    var responsePromise = fetch(`https://www.omdbapi.com/?apikey=20dc4c7f&s=${searchText}`);
-
-    function handleResponse(responseObj) {
-      return responseObj.json();
-    }
-
-    responsePromise
-      .then(handleResponse)
-      .then(function (data) {
-
-        displayMatches(data.Search);
-      });
+    $.get(`https://www.omdbapi.com/?apikey=20dc4c7f&s=${searchText}`)
+    .then(function (data) {
+      displayMatches(data.Search);
+    });
   }
 }
 
 function init() {
-  searchInput.addEventListener('keydown', getMovieData);
+  searchInput.keydown(getMovieData);
 }
 
 init();
